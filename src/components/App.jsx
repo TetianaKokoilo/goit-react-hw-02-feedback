4996425fbd40562a3d8fbd88d2efca2bea0baad7
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-21 16:33:31 +0200

    countTotalFeedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,23 +4,34 @@ export class App extends Component {
   static propTypes = {
     //
   };
+  constructor(props) {
+    super(props);
+    this.state = {
+      good: 0,
+      neutral: 3,
+      bad: 0,
+    };
+  }
 
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  countTotalFeedback = () => {
+    const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
+    return totalFeedback;
   };
 
   render() {
-    return <div>
-      <h2>Please leave feedback</h2>
-      <button type='button'>Good</button>
-      <button type='button'>Neutral</button>
-      <button type='button'>Bad</button>
-      <h2>Statistics</h2>
-      <p>Good <span>{this.state.good}</span></p>
-      <p>Neutral <span>{this.state.neutral}</span></p>
-      <p>Bad <span>{this.state.bad}</span></p>
-    </div>;
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+        <button type="button">Good</button>
+        <button type="button">Neutral</button>
+        <button type="button">Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+        <p>Total:</p>
+        <p>Positive feedback:</p>
+      </div>
+    );
   }
 }
