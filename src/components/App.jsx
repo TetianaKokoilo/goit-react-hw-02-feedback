de9c5460fe5b0b8a59a2ea89eaf75477b2cdca57
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-21 16:02:52 +0200

    add class App with step 1 for feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  static propTypes = {
+    //
+  };
+
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return <div>
+      <h2>Please leave feedback</h2>
+      <button type='button'>Good</button>
+      <button type='button'>Neutral</button>
+      <button type='button'>Bad</button>
+      <h2>Statistics</h2>
+      <p>Good <span>{this.state.good}</span></p>
+      <p>Neutral <span>{this.state.neutral}</span></p>
+      <p>Bad <span>{this.state.bad}</span></p>
+    </div>;
+  }
+}
