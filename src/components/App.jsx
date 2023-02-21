c6f382efdfa1ecdc5ccb428d132dbe5d4724b38d
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-21 17:46:53 +0200

    add Section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 
 export class App extends Component {
@@ -6,11 +7,11 @@ export class App extends Component {
     //
   };
 
-    state = {
-      good: 0,
-      neutral: 2,
-      bad: 2,
-    };
+  state = {
+    good: 0,
+    neutral: 2,
+    bad: 2,
+  };
 
   countTotalFeedback = () => {
     const totalFeedback = this.state.good + this.state.neutral + this.state.bad;
@@ -23,16 +24,21 @@ export class App extends Component {
   render() {
     return (
       <div>
-        <h2>Please leave feedback</h2>
+        <Section>
+          
+        </Section>
+        {/* <h2>Please leave feedback</h2>
         <button type="button">Good</button>
         <button type="button">Neutral</button>
         <button type="button">Bad</button>
-        <h2>Statistics</h2>
-        <Statistics
-          points={this.state}
-          onTotalCount={this.countTotalFeedback}
-          onPositivePercentage={this.countPositiveFeedbackPercentage}
-        />
+        <h2>Statistics</h2> */}
+        <Section title="Statsics">
+          <Statistics
+            points={this.state}
+            onTotalCount={this.countTotalFeedback}
+            onPositivePercentage={this.countPositiveFeedbackPercentage}
+          />
+        </Section>
       </div>
     );
   }
